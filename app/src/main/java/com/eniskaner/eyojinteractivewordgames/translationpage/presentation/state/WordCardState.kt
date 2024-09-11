4b0e088b322da7199a707d209f79b250699cab52
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-12 02:31:21 +0300

    add: WordCardState update state for word cards

---
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/presentation/state/WordCardState.kt
@@ -0,0 +1,8 @@
+package com.eniskaner.eyojinteractivewordgames.translationpage.presentation.state
+
+import com.eniskaner.eyojinteractivewordgames.translationpage.data.model.UIWordCard
+
+data class WordCardState(
+    val wordCardsList: List<UIWordCard> = emptyList(),
+    val learnedEnglishCardList: List<UIWordCard> = emptyList()
+)
