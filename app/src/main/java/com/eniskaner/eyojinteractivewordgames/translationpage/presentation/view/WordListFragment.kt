b6e84bafb65d9200879def51b6f30e024dda9349
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-12 02:33:00 +0300

    add: view model and follow the updates with flow

--- app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/presentation/view/WordListFragment.kt
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/presentation/view/WordListFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.eniskaner.eyojinteractivewordgames.R
 import com.eniskaner.eyojinteractivewordgames.common.base.BaseFragment
 import com.eniskaner.eyojinteractivewordgames.common.util.addCarouselEffect
+import com.eniskaner.eyojinteractivewordgames.common.util.launchAndRepeatWithViewLifecycle
 import com.eniskaner.eyojinteractivewordgames.databinding.FragmentWordListBinding
 import com.eniskaner.eyojinteractivewordgames.translationpage.data.model.UIWordCard
 import com.eniskaner.eyojinteractivewordgames.translationpage.data.model.UIWordCardProvider
 import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.adapter.CarouselClickListener
 import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.adapter.WordCarouselAdapter
+import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.viewmodel.SharedWordCardViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,6 +28,7 @@ class WordListFragment : BaseFragment<FragmentWordListBinding>(), CarouselClickL
 
     private val adapter by lazy { WordCarouselAdapter(this@WordListFragment) }
     private val navController: NavController by lazy { findNavController() }
+    private val sharedWordCardViewModel: SharedWordCardViewModel by viewModels()
 
     @Inject
     lateinit var uiWordCardProvider: UIWordCardProvider
@@ -34,15 +39,25 @@ class WordListFragment : BaseFragment<FragmentWordListBinding>(), CarouselClickL
         super.onViewCreated(view, savedInstanceState)
         initViewPager()
         binding.swipeRefreshLayoutWordList.setOnRefreshListener {
-            initViewPager()
+            getWordListData()
             binding.swipeRefreshLayoutWordList.isRefreshing = false
         }
+        getWordListData()
     }
 
     private fun initViewPager() {
         binding.viewPager.addCarouselEffect()
         binding.viewPager.adapter = adapter
-        adapter.submitList(uiWordCardProvider.addWords())
+    }
+
+    private fun getWordListData() {
+        launchAndRepeatWithViewLifecycle {
+            launch {
+                sharedWordCardViewModel.wordCardListState.collect { wordCardState ->
+                    adapter.submitList(wordCardState.wordCardsList.shuffled())
+                }
+            }
+        }
     }
 
     override fun wordCardClickListener(item: UIWordCard) {
