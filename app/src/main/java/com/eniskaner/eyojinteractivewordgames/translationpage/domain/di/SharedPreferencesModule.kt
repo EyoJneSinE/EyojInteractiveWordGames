11c0b117d8727da415d5adbb51e37c1e2d61afc1
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-12 02:30:27 +0300

    add: SharedPreferencesModule provides PrefUtils and WordCardRepository with hilt dependency injection

---
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/domain/di/SharedPreferencesModule.kt
@@ -0,0 +1,29 @@
+package com.eniskaner.eyojinteractivewordgames.translationpage.domain.di
+
+import android.content.Context
+import com.eniskaner.eyojinteractivewordgames.common.sharedpreferences.PrefUtils
+import com.eniskaner.eyojinteractivewordgames.translationpage.data.repo.WordCardRepositoryImpl
+import com.eniskaner.eyojinteractivewordgames.translationpage.domain.repo.WordCardRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPreferencesModule {
+
+    @Provides
+    @Singleton
+    fun providePrefUtils(@ApplicationContext context: Context): PrefUtils {
+        return PrefUtils(context = context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordCardRepository(prefUtils: PrefUtils): WordCardRepository {
+        return WordCardRepositoryImpl(prefUtils = prefUtils)
+    }
+}
