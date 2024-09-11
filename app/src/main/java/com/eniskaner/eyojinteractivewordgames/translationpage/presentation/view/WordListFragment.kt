f885745d497df709d79226227c6b36949b690676
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-12 00:10:58 +0300

    add: nav args when navigate to detail

--- app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/presentation/view/WordListFragment.kt
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translationpage/presentation/view/WordListFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.eniskaner.eyojinteractivewordgames.R
 import com.eniskaner.eyojinteractivewordgames.common.base.BaseFragment
 import com.eniskaner.eyojinteractivewordgames.common.util.addCarouselEffect
@@ -20,6 +23,7 @@ import javax.inject.Inject
 class WordListFragment : BaseFragment<FragmentWordListBinding>(), CarouselClickListener {
 
     private val adapter by lazy { WordCarouselAdapter(this@WordListFragment) }
+    private val navController: NavController by lazy { findNavController() }
 
     @Inject
     lateinit var uiWordCardProvider: UIWordCardProvider
@@ -29,6 +33,10 @@ class WordListFragment : BaseFragment<FragmentWordListBinding>(), CarouselClickL
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViewPager()
+        binding.swipeRefreshLayoutWordList.setOnRefreshListener {
+            initViewPager()
+            binding.swipeRefreshLayoutWordList.isRefreshing = false
+        }
     }
 
     private fun initViewPager() {
@@ -38,7 +46,14 @@ class WordListFragment : BaseFragment<FragmentWordListBinding>(), CarouselClickL
     }
 
     override fun wordCardClickListener(item: UIWordCard) {
+        navigateToWordDetails(item)
+    }
 
+    private fun navigateToWordDetails(item: UIWordCard) {
+        val bundle = bundleOf(
+            "uiWordCard" to item
+        )
+        navController.navigate(R.id.action_wordListFragment_to_wordDetailFragment, bundle)
     }
 
 }
