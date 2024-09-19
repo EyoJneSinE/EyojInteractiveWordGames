a955d250ad2874b14d9d1398410ed4a3cc5612a1
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-19 18:23:05 +0300

    add: LanguageCodeState for state flow

---
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translator/presentation/LanguageCodeState.kt
@@ -0,0 +1,7 @@
+package com.eniskaner.eyojinteractivewordgames.translator.presentation
+
+import com.eniskaner.eyojinteractivewordgames.translator.data.model.LanguageModel
+
+data class LanguageCodeState(
+    val languageCodes: List<LanguageModel> = emptyList()
+)
