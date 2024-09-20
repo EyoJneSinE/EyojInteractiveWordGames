439e2eafbf373b2fedc049a58f94fa86e7a5ad74
Enis Kaner <enis.d.kaner@gmail.com>
2024-09-20 04:48:35 +0300

    add: language code functionality for getting data and display on UI

--- app/src/main/java/com/eniskaner/eyojinteractivewordgames/translator/presentation/adapter/LanguageCodeAdapter.kt
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translator/presentation/adapter/LanguageCodeAdapter.kt
@@ -4,11 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.eniskaner.eyojinteractivewordgames.databinding.ItemLanguageCodeSingleChooseBinding
-import com.eniskaner.eyojinteractivewordgames.databinding.LearnedGermanItemCarouselBinding
-import com.eniskaner.eyojinteractivewordgames.translationpage.data.model.UIWordCard
-import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.adapter.CarouselClickListener
-import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.adapter.LearnedWordsCarouselDiffCallBack
-import com.eniskaner.eyojinteractivewordgames.translationpage.presentation.viewholder.LearnedGermanViewHolder
 import com.eniskaner.eyojinteractivewordgames.translator.data.model.LanguageModel
 import com.eniskaner.eyojinteractivewordgames.translator.presentation.viewholder.LanguageCodeViewHolder
 
--- app/src/main/java/com/eniskaner/eyojinteractivewordgames/translator/presentation/view/LanguageCodeFragment.kt
+++ app/src/main/java/com/eniskaner/eyojinteractivewordgames/translator/presentation/view/LanguageCodeFragment.kt
@@ -5,10 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.eniskaner.eyojinteractivewordgames.R
+import com.eniskaner.eyojinteractivewordgames.common.util.launchAndRepeatWithViewLifecycle
+import com.eniskaner.eyojinteractivewordgames.common.util.viewBinding
+import com.eniskaner.eyojinteractivewordgames.databinding.FragmentLanguageCodeBinding
+import com.eniskaner.eyojinteractivewordgames.translator.data.model.LanguageModel
+import com.eniskaner.eyojinteractivewordgames.translator.presentation.adapter.LanguageClickListener
+import com.eniskaner.eyojinteractivewordgames.translator.presentation.adapter.LanguageCodeAdapter
+import com.eniskaner.eyojinteractivewordgames.translator.presentation.viewmodel.LanguageViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class LanguageCodeFragment : Fragment() {
+class LanguageCodeFragment : Fragment(), LanguageClickListener {
+
+    private val binding by viewBinding(FragmentLanguageCodeBinding::bind)
+    private val viewModel: LanguageViewModel by viewModels()
+    private val adapter by lazy { LanguageCodeAdapter(this@LanguageCodeFragment) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        getLanguageCodeData()
+        observeLanguageCodeData()
+    }
+
+    private fun initRecyclerView() = with(binding) {
+        recyclerViewSingleChooseLanguageCode.layoutManager = LinearLayoutManager(requireContext())
+        recyclerViewSingleChooseLanguageCode.adapter = adapter
+    }
+
+    private fun getLanguageCodeData() {
+        viewModel.getLanguageCodes()
+    }
+
+    private fun observeLanguageCodeData() {
+        launchAndRepeatWithViewLifecycle {
+            launch {
+                viewModel.languageCodeState.collect { languageCodeState ->
+                    adapter.submitList(languageCodeState.languageCodes)
+                }
+            }
+        }
+    }
+
+    override fun languageCodeClickListener(item: LanguageModel, position: Int) {
+
+    }
 
 }
